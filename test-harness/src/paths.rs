76a5cf2b41968ccbc9d01fa6d4dd1513fca351fa
VitalyV1337 <vitalyvolozhinov@gmail.com>
2024-12-04 02:25:33 +0100

    Adjusting paths

--- test-harness/src/paths.rs
+++ test-harness/src/paths.rs
@@ -1,8 +1,8 @@
 pub const AMM_CONTRACT_BINARY_PATH: &str =
-    "../../contracts/mira_amm_contract/out/debug/mira_amm_contract.bin";
+    "contracts/mira_amm_contract/out/debug/mira_amm_contract.bin";
 pub const HOOK_CONTRACT_BINARY_PATH: &str =
-    "../../contracts/mira_validation_hook/out/debug/mira_validation_hook.bin";
+    "contracts/mira_validation_hook/out/debug/mira_validation_hook.bin";
 
 // Mocks
 pub const MOCK_TOKEN_CONTRACT_BINARY_PATH: &str =
-    "../../contracts/mocks/mock_token/out/debug/mock_token.bin";
+    "contracts/mocks/mock_token/out/debug/mock_token.bin";
